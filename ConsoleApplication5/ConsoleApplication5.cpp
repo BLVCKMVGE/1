4f4f14ac0eddfd30f02609fbc95ea0e3e584b7e2
BLVCKMVGE <nik3a@mail.ru>
2019-06-26 18:59:00 +0500

    1 zadacha

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+
+int main() {
+	int n, len;
+	std::cin >> n;
+	std::cin >> len;
+	char buffer[1000000 + 1];
+	std::cin >> buffer;
+	std::string s(buffer);
+	int pow = 1;
+	for (int i = 0; i < len; i++) {
+		pow *= 10;
+	}
+	std::vector<bool> was(pow, false);
+	int value = 0;
+	for (int i = 0; i + 1 < len && i < n; i++) {
+		value = value * 10 + s[i] - '0';
+	}
+	for (int i = len - 1; i < n; i++) {
+		value = value * 10 + s[i] - '0';
+		value %= pow;
+		if (was[value]) {
+			printf("YES");
+			return 0;
+		}
+		was[value] = true;
+	}
+	printf("NO");
+	return 0;
+}
